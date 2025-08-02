e7cfeb2e49476010b2591cedf99c3d3628848b0b
AyushRaj-10 <ayushrajranjan10@gmail.com>
2025-08-02 22:18:32 +0530

    Update dbConnection.js

--- Server/database/dbConnection.js
+++ Server/database/dbConnection.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+  mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        console.log("✅ Connected to database");
+      console.log("✅ Connected to MongoDB");
     })
     .catch((err) => {
-        console.error("❌ Error connecting to database:", err.message);
+      console.error("❌ MongoDB connection error:", err.message);
     });
 };
