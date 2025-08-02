d97b7264b5b5be933d52d47fcfa8ab65f1cc7423
AyushRaj-10 <ayushrajranjan10@gmail.com>
2025-08-02 23:34:46 +0530

    Update Nav.jsx

--- Client/src/components/Nav.jsx
+++ Client/src/components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     const navigate = useNavigate();
   return (
     <div className="w-full h-16 flex justify-between items-center px-8 bg-white shadow-md">
-      <h1 className="text-2xl font-bold text-gray-800">📝 Daily Journal</h1>
+      <h1 className="text-2xl font-bold text-gray-800">📝 InkSpace</h1>
       <div className="flex gap-4">
         <button onClick={() => navigate('/login')} className="px-6 py-2 border-2 border-green-500 text-green-600 rounded-full hover:bg-green-50 transition">Login</button>
         <button onClick={() => navigate('/register')} className="px-6 py-2 border-2 border-green-500 bg-green-600 text-white rounded-full hover:bg-green-700 transition">Register</button>
